f8c06b1f5eceec9cab4bb923ed438fd6f7ea0593
undeadj0e <evgeniykutik@gmail.com>
2025-04-14 01:23:14 +0200

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,10 @@ const activeChain = "ethereum";
 export default function App({ Component, pageProps }) {
   return (
     <ThirdwebProvider
-      clientId="963ed92e2f4b9d38b122c31c1a13433f"
       activeChain={activeChain}
+      sdkOptions={{
+        clientId: "963ed92e2f4b9d38b122c31c1a13433f",
+      }}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
